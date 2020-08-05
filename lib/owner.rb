3c937d200edd694b9eea92a07c742b1c4b6c4411
Chenyun Zhang <morningchenyun@gmail.com>
2020-08-05 16:24:11 -0400

    first round

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,36 @@
+require 'pry'
+
 class Owner
   # code goes here
+
+  attr_reader :species, :name
+  attr_accessor 
+
+  @@all =[]
+  @@count = 0
+
+  def initialize(name)
+    @species = "human"
+    @name = name
+    @@all << self
+    @@count+=1
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@count
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def self.reset_all
+    #clear every out from the array
+    @@all.clear
+    @@count = 0
+  end
 end
